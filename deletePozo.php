a357893b82e75b4a637da31e8657fcbd4a3fe701
rdjgl24 <77794134+rdjgl24@users.noreply.github.com>
2022-04-09 22:42:27 -0400

    Update deletePozo.php

--- deletePozo.php
+++ deletePozo.php
@@ -3,8 +3,8 @@
 include_once('db/conn.php');
 
 $id=$_GET['id'];
-$sql0="DELETE FROM valores  WHERE idPozo='$id'";
-$query0=mysqli_query($conn,$sql0);
+$sql_aux="DELETE FROM valores  WHERE idPozo='$id'";
+$query?aux=mysqli_query($conn,$sql_aux);
 
 $sql="DELETE FROM pozo  WHERE id='$id'";
 $query=mysqli_query($conn,$sql);
@@ -12,5 +12,5 @@ $query=mysqli_query($conn,$sql);
     if($query){
         Header("Location: menu.php");
     }
-    mysqli_close($con);
+    mysqli_close($conn);
 ?>
